954426eac987bd6b3b98fa1788d7711900532339
PatilVeda <vedantpatil00h@gmail.com>
2025-07-24 18:42:26 +0530

    some new file

---
+++ Comments.py
@@ -0,0 +1,83 @@
+# print("helloworld")
+# print("hello vedant")
+
+# # comment in python
+# # print("hello vedant")  
+ 
+# # print("hello vedant")
+ 
+
+# # # #  variable in Python # # #
+
+
+# a =5
+# print(a)
+# b = 66
+# print(b) 
+
+# a =5.2
+# print(a)
+
+# c= False
+
+# print(c)
+
+
+# d = "harry"
+# print(d)
+
+# e = None
+# print(e)
+
+# f = "5"
+# print(int(f)+1)
+
+# g = 5
+# print(g+1)
+
+# Operator
+
+# num1 =10
+# num2 =2
+
+# #Arithmetic operator
+# print("num1 + num2 is", num1+num2)
+# print("num1 - num2 is", num1-num2)
+# print("num1 * num2 is", num1*num2)
+# print("num1 / num2 is", num1/num2)
+# print("num1 / num2 is", num1//num2)
+# print("num1 / num2 is", num1**num2)
+
+
+# #  assignment operator
+# a = 4
+# a -= 2
+# a *= 2
+# a -= 2
+# a -= 2
+# print(a)
+
+
+# comprasion operator 
+x = 8
+y = 3
+z = 8
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x==y)
+
+
+#logical operator
+# print(x==z  and x<y)
+
+# print(x==z or x<y)
+# print(not(False))
+# print(not(True))
+
+
+
+
+
+
+
---
+++ new.py
@@ -0,0 +1,14 @@
+# print("hello world")
+
+
+num1=int(input("enter a first number  ",))
+num2=int(input("enter a second number  ",))
+print(num1)
+print(num2)
+if num1 > num2:
+    print("The num1 is gretear than zero",num1)
+elif num1 < num2 :
+    print("the num2 number are equal",num2)   
+elif num1==num2:
+    print("Both Are equal",num1,num2)
+
