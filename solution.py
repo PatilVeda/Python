36d6ec75c1dccc087ae283ed78cd48ac7a2974ad
PatilVeda <vedantpatil00h@gmail.com>
2025-07-28 14:41:14 +0530

    new commit

---
+++ solution.py
@@ -0,0 +1,60 @@
+#basic class and object
+
+#class is a blueprint of the object
+#creating class
+
+# class Student:
+#     name = "karan"
+
+
+# #creating object (instance)
+
+
+# s1 =Student()
+# print(s1.name)
+
+# class Car:
+#     color = "blue"
+#     model = "benz"
+
+# car1 = Car()  #constructer
+# print(car1.color)  
+# print(car1.model)  
+
+
+
+#constructor
+
+# _init__function
+
+
+# All classes have a function called  _init_(),which is always executed when the object is being initialized.
+
+
+
+# class Student:
+
+#    #default constructors created by python itself
+#     def __init__(self,fullname,marks):
+       
+#        pass
+  
+
+#   #parameterized constrcter
+#     def __init__(self,fullname,marks):
+#         self.name=fullname
+#         self.marks=marks
+        
+#         print("creating new student")
+ 
+# s1 = Student("karan",67)
+# print(s1.name,s1.marks)
+
+
+# s2 =Student("arjun",87)
+# print(s2.name,s2.marks)
+
+
+#class & instance attribute
+
+
