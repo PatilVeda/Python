1fc79ee9da9357e1dd83100e5b2081ba50524c7e
PatilVeda <vedantpatil00h@gmail.com>
2025-07-28 15:40:11 +0530

    sample

--- solution.py
+++ solution.py
@@ -85,22 +85,91 @@
 #method are function are belong to objects
 
 
-class Student:
+# class Student:
 
-    college_name = "ABC COLLEGE"
+#     college_name = "ABC COLLEGE"
 
 
-    def __init__(self,name,marks):
-        self.name=name  #obj attr > class attr
-        self.marks=marks
+#     def __init__(self,name,marks):
+#         self.name=name  #obj attr > class attr
+#         self.marks=marks
         
-    def welcome(self):
-        print("welcome student",self.college_name)
+#     def welcome(self):
+#         print("welcome student",self.college_name)
 
-    def get_marks(self):
-        return self.marks
+#     def get_marks(self):
+#         return self.marks
  
-s1 = Student("karan",67)
-s1.welcome()
-print(s1.get_marks())
+# s1 = Student("karan",67)
+# s1.welcome()
+# print(s1.get_marks())
 
+
+# class Student:
+#     def __init__(self,name,marks):
+#         self.name =name
+#         self.marks = marks
+
+#     def get_avg(self):
+#        sum =0
+#        for value in self.marks:
+#            sum +=value  
+#        print("hii",self.name,"your avg score is :",sum/5)   
+
+# s1 =Student("ved",[23,34,34,44,54])
+# s1.get_avg()
+
+
+# s1.name = "hey"
+# s1.get_avg()
+
+
+
+
+
+
+#Static method 
+#are method dont use self parameter
+
+
+
+# class Student:
+#     def __init__(self,name,marks):
+#         self.name =name
+#         self.marks = marks
+
+
+
+#     @staticmethod    
+#     def hello():
+#         print("hello")    
+
+
+
+
+
+#     def get_avg(self):
+#        sum =0
+#        for value in self.marks:
+#            sum +=value  
+#        print("hii",self.name,"your avg score is :",sum/5)   
+
+# s1 =Student("ved",[23,34,34,44,54])
+# s1.get_avg()
+
+
+
+
+#Abstration
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk =False
+        self.clutch=  False
+    def start(self):
+        self.clutch =True 
+        self.acc =  True 
+        print("car started") 
+
+Car1 = Car()
+Car1.start()        
