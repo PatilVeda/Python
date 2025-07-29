4d328a19d18b81cd802b18c4327818dbf421250d
PatilVeda <vedantpatil00h@gmail.com>
2025-07-29 17:33:26 +0530

    simple push

---
+++ exception.py
@@ -0,0 +1,5 @@
+try:
+   a = int(input("enter the number: "))
+   print(a + 3)
+except Exception as e:
+   print("some error occured",e)   
---
+++ match.py
@@ -0,0 +1,17 @@
+#it basically added python 3.1
+
+
+a= int(input("enter a number"))
+match a:
+    case 1:
+        print("case is 1 ")
+    case 2:
+        print("case is 2")    
+    case 12:
+        print("case is 12")    
+    case 22:
+        print("case is 22")    
+    case 34:
+        print("case is 34") 
+    case _:
+        print("No match found")       
