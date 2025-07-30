12e8908ca876f544b17ed2210d2c75c76b221fa5
PatilVeda <vedantpatil00h@gmail.com>
2025-07-30 10:39:16 +0530

    simple

--- exception.py
+++ exception.py
@@ -1,5 +1,12 @@
+# try:
+#    a = int(input("enter the number: "))
+#    print(a + 3)
+# except Exception as e:
+   # print("some error occured",e)   
+
+
 try:
    a = int(input("enter the number: "))
    print(a + 3)
 except Exception as e:
-   print("some error occured",e)   
+   print("some error occured",e)  
