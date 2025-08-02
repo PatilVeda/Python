cdd8ac564fa184448b213357987d7a6defa62e6c
PatilVeda <vedantpatil00h@gmail.com>
2025-08-02 23:06:07 +0530

    simple

--- anagram.py
+++ anagram.py
@@ -5,17 +5,22 @@
 # s2 = input("Enter second string: ")
 # print("Anagram" if is_anagram(s1, s2) else "Not anagram")
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+#     return True
 
-n = int(input("Enter a number: "))
-total = 0
-for i in range(2, n + 1):
-    if is_prime(i):
-        total += i
-print("Sum of primes:", total)
+# n = int(input("Enter a number: "))
+# total = 0
+# for i in range(2, n + 1):
+#     if is_prime(i):
+#         total += i
+# print("Sum of primes:", total)
+lst = list(map(int, input("Enter numbers: ").split()))
+rev = []
+for i in range(len(lst)-1, -1, -1):
+    rev.append(lst[i])
+print("Reversed list:", rev)
