710fa5174e86a40b6db2848aa5e1cd4c3996f566
PatilVeda <vedantpatil00h@gmail.com>
2025-08-05 21:44:17 +0530

    simple commit

--- anagram.py
+++ anagram.py
@@ -5,13 +5,13 @@
 # s2 = input("Enter second string: ")
 # print("Anagram" if is_anagram(s1, s2) else "Not anagram")
 
-# def is_prime(n):
-#     if n < 2:
-#         return False
-#     for i in range(2, n):
-#         if n % i == 0:
-#             return False
-#     return True
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 # n = int(input("Enter a number: "))
 # total = 0
@@ -24,6 +24,6 @@
 # for i in range(len(lst)-1, -1, -1):
 #     rev.append(lst[i])
 # print("Reversed list:", rev)
-nums = [1, 2, 3, 4]
-squares = list(map(lambda x: x * x, nums))
-print(squares)  # Output: [1, 4, 9, 16]
+# nums = [1, 2, 3, 4]
+# squares = list(map(lambda x: x * x, nums))
+# print(squares)  # Output: [1, 4, 9, 16]
