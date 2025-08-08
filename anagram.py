e186aa25033cd207f50a341e27427e223d4bea74
PatilVeda <vedantpatil00h@gmail.com>
2025-08-08 20:28:30 +0530

    simple

--- anagram.py
+++ anagram.py
@@ -13,17 +13,17 @@ def is_prime(n):
             return False
     return True
 
-n = int(input("Enter a number: "))
-total = 0
-for i in range(2, n + 1):
-    if is_prime(i):
-        total += i
-print("Sum of primes:", total)
-lst = list(map(int, input("Enter numbers: ").split()))
-rev = []
-for i in range(len(lst)-1, -1, -1):
-    rev.append(lst[i])
-print("Reversed list:", rev)
-nums = [1, 2, 3, 4]
-squares = list(map(lambda x: x * x, nums))
-print(squares)  # Output: [1, 4, 9, 16]
+# n = int(input("Enter a number: "))
+# total = 0
+# for i in range(2, n + 1):
+#     if is_prime(i):
+#         total += i
+# print("Sum of primes:", total)
+# lst = list(map(int, input("Enter numbers: ").split()))
+# rev = []
+# for i in range(len(lst)-1, -1, -1):
+#     rev.append(lst[i])
+# print("Reversed list:", rev)
+# nums = [1, 2, 3, 4]
+# squares = list(map(lambda x: x * x, nums))
+# print(squares)  # Output: [1, 4, 9, 16]
