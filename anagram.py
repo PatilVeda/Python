9c1a25001cfb854823da6ec7a2137a14ee45bc54
PatilVeda <vedantpatil00h@gmail.com>
2025-08-10 21:59:06 +0530

    simple

--- anagram.py
+++ anagram.py
@@ -1,9 +1,9 @@
-# def is_anagram(s1, s2):
-#     return sorted(s1) == sorted(s2)
+def is_anagram(s1, s2):
+    return sorted(s1) == sorted(s2)
 
-# s1 = input("Enter first string: ")
-# s2 = input("Enter second string: ")
-# print("Anagram" if is_anagram(s1, s2) else "Not anagram")
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+print("Anagram" if is_anagram(s1, s2) else "Not anagram")
 
 # def is_prime(n):
 #     if n < 2:
