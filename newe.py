cec5e01d71c573a1bd6aa9a31e6322a38da9ea33
PatilVeda <vedantpatil00h@gmail.com>
2025-09-13 20:04:05 +0530

    simple

---
+++ newe.py
@@ -0,0 +1,13 @@
+
+# name= input("entr your name")
+# name="sudhir"
+# env = "dev"
+# print("hello" ,name)
+
+
+num1= int(input("enter the number"))
+num2= int(input("enter the number"))
+
+
+sum_of_num = num1+num2
+print(sum_of_num)
