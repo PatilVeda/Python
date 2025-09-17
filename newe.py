08c01552448f52e615097e649dfb3cc972127180
PatilVeda <vedantpatil00h@gmail.com>
2025-09-17 11:40:14 +0530

    simple

--- newe.py
+++ newe.py
@@ -1,13 +1,13 @@
 
-# name= input("entr your name")
-# name="sudhir"
-# env = "dev"
-# print("hello" ,name)
+# # name= input("entr your name")
+# # name="sudhir"
+# # env = "dev"
+# # print("hello" ,name)
 
 
-num1= int(input("enter the number"))
-num2= int(input("enter the number"))
+# num1= int(input("enter the number"))
+# num2= int(input("enter the number"))
 
 
-sum_of_num = num1+num2
-print(sum_of_num)
+# sum_of_num = num1+num2
+# print(sum_of_num)
